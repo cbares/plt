af576cb2b95d43c00f0d6cfb334dfa77f15d6e65
maxime <maximemarroufin@gmail.com>
2020-12-20 19:45:55 +0100

    creation

---
+++ src/shared/state/Card.cpp
@@ -0,0 +1,19 @@
+#include "Card.h"
+
+using namespace state;
+
+Card::Card (std::string name, std::shared_ptr<Ressources> cost, std::shared_ptr<Ressources> gain){
+
+}
+
+Card::Card (Json::Value value){
+
+}
+
+Json::Value Card::serialize (){
+
+}
+
+void Card::unserialize (Json::Value value){
+
+}
