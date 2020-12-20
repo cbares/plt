127500f6e1d847ac56d8028546765d17cfe302ac
maxime <maximemarroufin@gmail.com>
2020-12-20 21:35:03 +0100

    added pick implementation

---
+++ src/shared/state/Player.cpp
@@ -0,0 +1,37 @@
+#include "Player.h"
+
+using namespace state;
+using namespace std;
+
+Player::Player(){
+    this->ressources = make_shared<Ressources>();
+}
+
+Player::Player(Json::Value value){
+    this->unserialize(value);
+}
+
+
+void Player::earnIncome (){
+    this->ressources->stone += this->ressources->stoneIncome;
+    this->ressources->wood += this->ressources->woodIncome;
+    this->ressources->water += this->ressources->waterIncome;
+    this->ressources->victoryPoint += this->ressources->victoryPointIncome;
+}
+
+void Player::pick(std::shared_ptr<Card> card){
+    this->ressources->sub(card->cost);
+    this->ressources->add(card->gain);
+}
+
+Json::Value Player::serialize(){
+    Json::Value value;
+
+    value["ressources"] = this->ressources->serialize();
+
+    return value;
+}
+
+void Player::unserialize(Json::Value value){
+    this->ressources = make_shared<Ressources>(value["ressources"]);
+}
