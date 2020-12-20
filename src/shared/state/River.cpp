de6977de8567bad7d281d31e5a42b9d5adc96113
maxime <maximemarroufin@gmail.com>
2020-12-20 23:01:31 +0100

    Added River methods

---
+++ src/shared/state/River.cpp
@@ -0,0 +1,27 @@
+#include "River.h"
+
+using namespace state;
+
+River::River(int tier){
+
+}
+
+Json::Value River::serialize(){
+
+}
+
+void River::unserialize(Json::Value value){
+
+}
+
+std::shared_ptr<Card> River::popCard(){
+
+}
+
+void River::addCard(std::shared_ptr<Card>){
+
+}
+
+void River::refill(){
+
+}
