e0df91b82f6ffc42b8354e4bd38700b37a9767b7
maxime <maximemarroufin@gmail.com>
2020-12-20 18:49:52 +0100

    renamed and add add and sub definition

--- src/shared/state/Ressources.cpp
+++ src/shared/state/Ressources.cpp
@@ -14,10 +14,24 @@ Ressources::Ressources(int stone, int stoneIncome, int water, int waterIncome, i
 }
 
 void Ressources::add (std::shared_ptr<Ressources> ressources){
-
+    this->stone += ressources->stone;
+    this->stoneIncome += ressources->stoneIncome;
+    this->water += ressources->water;
+    this->waterIncome += ressources->waterIncome;
+    this->wood += ressources->wood;
+    this->woodIncome += ressources->woodIncome;
+    this->victoryPoint += ressources->victoryPoint;
+    this->victoryPointIncome += ressources->victoryPointIncome;
 }
 void Ressources::sub (std::shared_ptr<Ressources> ressources){
-
+    this->stone -= ressources->stone;
+    this->stoneIncome -= ressources->stoneIncome;
+    this->water -= ressources->water;
+    this->waterIncome -= ressources->waterIncome;
+    this->wood -= ressources->wood;
+    this->woodIncome -= ressources->woodIncome;
+    this->victoryPoint -= ressources->victoryPoint;
+    this->victoryPointIncome -= ressources->victoryPointIncome;
 }
 bool Ressources::isGreater(std::shared_ptr<Ressources> ressources){
     return false;
