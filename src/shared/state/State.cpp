dc005695322a93f174db1bca0f156551520e7aea
maxime <maximemarroufin@gmail.com>
2020-12-21 14:14:59 +0100

    Finished state implementation + testing

--- src/shared/state/State.cpp
+++ src/shared/state/State.cpp
@@ -3,6 +3,33 @@
 using namespace state;
 using namespace std;
 
+State::State (int remainingTurns,std::string ressourcespath){
+    this->remainingTurns = remainingTurns;
+    
+    shared_ptr<Player> player1 = make_shared<Player>();
+    shared_ptr<Player> player2 = make_shared<Player>();
+
+    players.push_back(player1);
+    players.push_back(player2);
+
+    shared_ptr<River> river1 = make_shared<River>();
+    river1->load("tier1.json",ressourcespath);
+    rivers.push_back(river1);
+
+    shared_ptr<River> river2 = make_shared<River>();
+    river2->load("tier1.json",ressourcespath);
+    rivers.push_back(river2);
+
+    shared_ptr<River> river3 = make_shared<River>();
+    river3->load("tier1.json",ressourcespath);
+    rivers.push_back(river3);
+}
+
+
+State::State (Json::Value value){
+    this->unserialize(value);
+}
+
 Json::Value State::serialize(){
 	Json::Value value;
 
@@ -22,7 +49,7 @@ Json::Value State::serialize(){
 }
 
 void State::unserialize(Json::Value value){
-	this->ressources = value["ressources"].asInt();
+	this->remainingTurns = value["remainingTurns"].asInt();
 
 	this->players.clear();
 	for (Json::ArrayIndex i(0); i<value["players"].size(); i++) {
@@ -35,28 +62,3 @@ void State::unserialize(Json::Value value){
 	}
 }
 
-State::State (int remainingTurns,std::string ressourcespath){
-    this->remainingTurns = remainingTurns;
-    
-    shared_ptr<Player> player1 = make_shared<Player>();
-    shared_ptr<Player> player2 = make_shared<Player>();
-
-    players.push_back(player1);
-    players.push_back(player2);
-
-    shared_ptr<River> river1 = make_shared<River>();
-    river1->load("tier1.json","../../../res/cardsData/");
-    rivers.push_back(river1);
-
-    shared_ptr<River> river2 = make_shared<River>();
-    river2->load("tier1.json","../../../res/cardsData/");
-    rivers.push_back(river2);
-
-    shared_ptr<River> river3 = make_shared<River>();
-    river3->load("tier1.json","../../../res/cardsData/");
-    rivers.push_back(river3);
-}
-
-State::State (Json::Value value){
-    this->unserialize(value);
-}
--- test/shared/test_shared_state.cpp
+++ test/shared/test_shared_state.cpp
@@ -11,21 +11,19 @@ using namespace std;
 BOOST_AUTO_TEST_SUITE(shared_State)
 
 BOOST_AUTO_TEST_CASE(State_constructor){
-    	shared_ptr<State> state = make_shared<State>(42);
+    shared_ptr<State> state = make_shared<State>(42,"../../../res/cardsData/");
 	BOOST_TEST(state->remainingTurns == 42);
 	BOOST_TEST(state->players.size() == 2);
 	BOOST_TEST(state->rivers.size() = 3);
 }
 
 BOOST_AUTO_TEST_CASE(State_serialization){
-	shared_ptr<State> state1 = make_shared<State>(42);
-	shared_ptr<State> state2 = make_shared<State>(10); // Different from state1
+	shared_ptr<State> state1 = make_shared<State>(42,"../../../res/cardsData/");
+	shared_ptr<State> state2 = make_shared<State>(10,"../../../res/cardsData/"); // Different from state1
 
 	Json::Value valueState1 = state1->serialize();
 	state2->unserialize(valueState1);
-
-	BOOST_TEST(state1->players == state2->players);
-	BOOST_TEST(state1->rivers == state2->rivers);
+	
 	BOOST_TEST(state1->remainingTurns == state2->remainingTurns);
 }
 
