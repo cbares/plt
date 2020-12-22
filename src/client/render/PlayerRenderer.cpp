004187bb354b096e5f4b245a44ed326f926a7297
maxime <maximemarroufin@gmail.com>
2020-12-22 17:20:38 +0100

    use icon on player renderer

--- src/client/render/PlayerRenderer.cpp
+++ src/client/render/PlayerRenderer.cpp
@@ -22,6 +22,23 @@ PlayerRenderer::PlayerRenderer(std::shared_ptr<state::Player> player, sf::Vector
     this->texture.loadFromFile("res/textures/denim.png");
 
     this->font.loadFromFile("res/fonts/arial.ttf");
+
+    sf::Texture _waterTexture;
+    _waterTexture.loadFromFile("res/textures/water.png");
+    _waterTexture.setSmooth(true);
+    this->ressourcesTextures["water"] = _waterTexture;
+    sf::Texture _stoneTexture;
+    _stoneTexture.loadFromFile("res/textures/stone.png");
+    _stoneTexture.setSmooth(true);
+    this->ressourcesTextures["stone"] = _stoneTexture;
+    sf::Texture _woodTexture;
+    _woodTexture.loadFromFile("res/textures/wood.png");
+    _woodTexture.setSmooth(true);
+    this->ressourcesTextures["wood"] = _woodTexture;
+    sf::Texture _crownTexture;
+    _crownTexture.loadFromFile("res/textures/crown.png");
+    _crownTexture.setSmooth(true);
+    this->ressourcesTextures["crown"] = _crownTexture;
 }
 
 void PlayerRenderer::draw(sf::RenderTarget &target, sf::RenderStates states) const{
@@ -31,35 +48,67 @@ void PlayerRenderer::draw(sf::RenderTarget &target, sf::RenderStates states) con
     _sprite.setScale(400/_sprite.getLocalBounds().width,499/_sprite.getLocalBounds().height);
     _sprite.setPosition(_position.x,_position.y);
 
+    _position += sf::Vector2f(10,30);
+
     target.draw(_sprite);
 
     sf::Text _nameText(string("Player :") + this->name,this->font, 30);
-    
-    _nameText.setPosition(_position.x+10,_position.y+30);
+    _nameText.setPosition(_position.x,_position.y);
     target.draw(_nameText);
 
-    sf::Text _stoneText(string("Stone :") + to_string(this->ressources->stone) + 
-                    string("(") + dif2string(this->ressources->stoneIncome) + string(")\n"),this->font, 30);
-    
-    _stoneText.setPosition(_position.x+10,_position.y+90);
-    target.draw(_stoneText);
-    
-    
-    sf::Text _woodText(string("wood :") + to_string(this->ressources->wood) + 
-                    string("(") + dif2string(this->ressources->woodIncome) + string(")\n"),this->font, 30);
+    _position += sf::Vector2f(0,90);
 
-    _woodText.setPosition(_position.x+10,_position.y+150);
+    sf::Sprite _stoneSprite(this->ressourcesTextures.find("stone")->second);
+    _stoneSprite.setScale(50/_stoneSprite.getLocalBounds().width,50/_stoneSprite.getLocalBounds().height);
+    _stoneSprite.setPosition(_position.x,_position.y);
+    target.draw(_stoneSprite);
+
+    _position += sf::Vector2f(_stoneSprite.getGlobalBounds().width,0);
+    sf::Text _stoneText(to_string(this->ressources->stone) + 
+                    string("(") + dif2string(this->ressources->stoneIncome) + string(")\n"),this->font, 30);
+    _stoneText.setPosition(_position.x,_position.y);
+    target.draw(_stoneText);
+
+    _position = sf::Vector2f(this->getPosition().x+10,_stoneText.getGlobalBounds().top+_stoneText.getGlobalBounds().height+10);
+    
+
+    sf::Sprite _woodSprite(this->ressourcesTextures.find("wood")->second);
+    _woodSprite.setScale(50/_woodSprite.getLocalBounds().width,50/_woodSprite.getLocalBounds().height);
+    _woodSprite.setPosition(_position.x,_position.y);
+    target.draw(_woodSprite);
+
+    _position += sf::Vector2f(_woodSprite.getGlobalBounds().width,0);
+    sf::Text _woodText(to_string(this->ressources->wood) + 
+                    string("(") + dif2string(this->ressources->woodIncome) + string(")\n"),this->font, 30);
+    _woodText.setPosition(_position.x,_position.y);
     target.draw(_woodText);
 
-    sf::Text _waterText(string("water :") + to_string(this->ressources->water) + 
-                    string("(") + dif2string(this->ressources->waterIncome) + string(")\n"),this->font, 30);
+    _position = sf::Vector2f(this->getPosition().x+10,_woodText.getGlobalBounds().top+_woodText.getGlobalBounds().height+10);
 
-    _waterText.setPosition(_position.x+10,_position.y+210);
+    sf::Sprite _waterSprite(this->ressourcesTextures.find("water")->second);
+    _waterSprite.setScale(50/_waterSprite.getLocalBounds().width,50/_waterSprite.getLocalBounds().height);
+    _waterSprite.setPosition(_position.x,_position.y);
+    target.draw(_waterSprite);
+
+    _position += sf::Vector2f(_waterSprite.getGlobalBounds().width,0);
+    sf::Text _waterText(to_string(this->ressources->water) + 
+                    string("(") + dif2string(this->ressources->waterIncome) + string(")\n"),this->font, 30);
+    _waterText.setPosition(_position.x,_position.y);
     target.draw(_waterText);
 
-    sf::Text _victoryPointText(string("VictoryPoint :") + to_string(this->ressources->victoryPoint) + 
-                    string("(") + dif2string(this->ressources->victoryPointIncome) + string(")\n"),this->font, 30);
+    _position = sf::Vector2f(this->getPosition().x+10,_waterSprite.getGlobalBounds().top+_waterSprite.getGlobalBounds().height+10);
 
-    _victoryPointText.setPosition(_position.x+10,_position.y+270);
-    target.draw(_victoryPointText);
+    sf::Sprite _victoryPointSprite(this->ressourcesTextures.find("crown")->second);
+    _victoryPointSprite.setScale(50/_victoryPointSprite.getLocalBounds().width,50/_victoryPointSprite.getLocalBounds().height);
+    _victoryPointSprite.setPosition(_position.x,_position.y);
+    target.draw(_victoryPointSprite);
+
+    _position += sf::Vector2f(_victoryPointSprite.getGlobalBounds().width,0);
+    sf::Text __victoryPointText(to_string(this->ressources->victoryPoint) + 
+                    string("(") + dif2string(this->ressources->victoryPointIncome) + string(")\n"),this->font, 30);
+    __victoryPointText.setPosition(_position.x,_position.y);
+    target.draw(__victoryPointText);
+
+    _position = sf::Vector2f(this->getPosition().x+10,_victoryPointSprite.getGlobalBounds().top+_victoryPointSprite.getGlobalBounds().height+10);
+
 }
