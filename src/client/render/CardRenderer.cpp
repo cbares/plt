7592b865cd609534cb821f16251b54319c4554bf
maxime <maximemarroufin@gmail.com>
2020-12-22 12:29:24 +0100

    added cost to rendering

--- src/client/render/CardRenderer.cpp
+++ src/client/render/CardRenderer.cpp
@@ -19,6 +19,23 @@ CardRenderer::CardRenderer(std::shared_ptr<state::Card> card, sf::Vector2f posit
     this->name = card->name;
     this->cost = card->cost;
     this->gain = card->gain;
+
+    sf::Texture _waterTexture;
+    _waterTexture.loadFromFile("res/textures/water.png");
+    _waterTexture.setSmooth(true);
+    this->ressourcesTextures["water"] = _waterTexture;
+    sf::Texture _stoneTexture;
+    _stoneTexture.loadFromFile("res/textures/stone.png");
+    _stoneTexture.setSmooth(true);
+    this->ressourcesTextures["stone"] = _stoneTexture;
+    sf::Texture _woodTexture;
+    _woodTexture.loadFromFile("res/textures/wood.png");
+    _woodTexture.setSmooth(true);
+    this->ressourcesTextures["wood"] = _woodTexture;
+    sf::Texture _crownTexture;
+    _crownTexture.loadFromFile("res/textures/crown.png");
+    _crownTexture.setSmooth(true);
+    this->ressourcesTextures["crown"] = _crownTexture;
 }
 
 void CardRenderer::draw(sf::RenderTarget &target, sf::RenderStates states) const{
@@ -30,12 +47,54 @@ void CardRenderer::draw(sf::RenderTarget &target, sf::RenderStates states) const
     _borderSprite.setColor(sf::Color::Black);
     target.draw(_borderSprite);
 
-    sf::Sprite _sprite(this->texture);
-    _sprite.setScale(90/_sprite.getLocalBounds().width,120/_sprite.getLocalBounds().height);
-    _sprite.setPosition(_position.x+5,_position.y+5);
-    target.draw(_sprite);
+    _position += sf::Vector2f(5,5);
+    // Card name part
+    sf::Sprite _nameSprite(this->texture);
+    _nameSprite.setScale(90/_nameSprite.getLocalBounds().width,20/_nameSprite.getLocalBounds().height);
+    _nameSprite.setPosition(_position.x,_position.y);
+    target.draw(_nameSprite);
 
     sf::Text _nameText(this->name,this->font, 15);
-    _nameText.setPosition(_position.x+5,_position.y+5);
+    _nameText.setPosition(_position.x,_position.y);
     target.draw(_nameText);
+
+    _position += sf::Vector2f(0,20);
+
+    // Card cost part
+    if(cost->stone !=0){
+        sf::Text _nameText(to_string(this->cost->stone),this->font, 15);
+        _nameText.setPosition(_position.x,_position.y);
+        target.draw(_nameText);
+        _position += sf::Vector2f(_nameText.getLocalBounds().width,0);
+        sf::Sprite _spriteStone(this->ressourcesTextures.find("stone")->second);
+        _spriteStone.setScale(15.36/_spriteStone.getLocalBounds().width,15.36/_spriteStone.getLocalBounds().height);
+        _spriteStone.setPosition(_position.x,_position.y+2);
+        target.draw(_spriteStone);
+        _position += sf::Vector2f(_spriteStone.getLocalBounds().width,0);
+    }
+    if(cost->wood !=0){
+        sf::Text _nameText(to_string(this->cost->wood),this->font, 15);
+        _nameText.setPosition(_position.x,_position.y);
+        target.draw(_nameText);
+        _position += sf::Vector2f(_nameText.getLocalBounds().width,0);
+        sf::Sprite _spriteWood(this->ressourcesTextures.find("wood")->second);
+        _spriteWood.setScale(15.36/_spriteWood.getLocalBounds().width,15.36/_spriteWood.getLocalBounds().height);
+        _spriteWood.setPosition(_position.x,_position.y+2);
+        target.draw(_spriteWood);
+        _position += sf::Vector2f(_spriteWood.getLocalBounds().width,0);
+    }
+    if(cost->water !=0){
+        sf::Text _nameText(to_string(this->cost->water),this->font, 15);
+        _nameText.setPosition(_position.x,_position.y);
+        target.draw(_nameText);
+        _position += sf::Vector2f(_nameText.getLocalBounds().width,0);
+        sf::Sprite _spriteWater(this->ressourcesTextures.find("water")->second);
+        _spriteWater.setScale(15.36/_spriteWater.getLocalBounds().width,15.36/_spriteWater.getLocalBounds().height);
+        _spriteWater.setPosition(_position.x,_position.y+2);
+        target.draw(_spriteWater);
+        _position += sf::Vector2f(_spriteWater.getLocalBounds().width,0);
+    }
+
+    // Card cost part
+
 }
