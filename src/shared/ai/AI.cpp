a04e950980857bec49beb03183f82048b75b6831
maxime <maximemarroufin@gmail.com>
2020-12-25 15:00:47 +0100

    Added AI methods implementation

---
+++ src/shared/ai/AI.cpp
@@ -0,0 +1,26 @@
+#include "AI.h"
+
+using namespace ai;
+using namespace std;
+
+void AI::updateState (std::shared_ptr<state::State> state){
+    this->state = state;
+}
+
+void AI::validCommandsInit (){
+    this->validCommands.clear();
+    vector<shared_ptr<state::River>> rivers = state->rivers;
+
+    for(uint i_rivers = 0;i_rivers < rivers.size();i_rivers++){
+        shared_ptr<state::River> river = rivers[i_rivers];
+        vector<shared_ptr<state::Card>> cards = river->cards;
+
+        for(uint i_cards = 0;i_cards < cards.size();i_cards++){
+            shared_ptr<state::Card> card = cards[i_cards];
+            shared_ptr<engine::Command> command = make_shared<engine::PickCommand>(i_rivers,i_cards,this->player->name);
+            if(command->verify(this->state)){
+                validCommands.push_back(command);    
+            }
+        }
+    }
+}
---
+++ src/shared/ai/RandomAI.cpp
@@ -0,0 +1,8 @@
+#include "RandomAI.h"
+
+using namespace ai;
+using namespace std;
+
+std::shared_ptr<engine::Command> RandomAI::getCommand (){
+
+}
