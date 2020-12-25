8def0a7a6c331ecf028d94e034add5a5c6da1eba
maxime <maximemarroufin@gmail.com>
2020-12-25 15:51:57 +0100

    added getCommand implementation

--- src/shared/ai/RandomAI.cpp
+++ src/shared/ai/RandomAI.cpp
@@ -4,7 +4,7 @@ using namespace ai;
 using namespace std;
 
 std::shared_ptr<engine::Command> RandomAI::getCommand (){
-
+    return validCommands[rand()%validCommands.size()];
 }
 
 RandomAI::RandomAI (std::shared_ptr<state::Player> player) : AI(player){
