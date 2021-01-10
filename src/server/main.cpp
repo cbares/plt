f8e15a36bbd141e3bf6907ce39aaa0263e047c2d
maxime <maximemarroufin@gmail.com>
2021-01-10 19:02:35 +0100

    Init of branch

---
+++ src/server/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <state.h>
+#include <server.h>
+#include <cstring>
+#include <random>
+
+using namespace std;
+using namespace state;
+using namespace server;
+
+int main(int argc,char* argv[])
+{   
+    srand(time(NULL));
+
+    return 0;
+}
