8403f93859a64b63801fe12c027e23276f426334
Alban Benmouffek <albanben@protonmail.com>
2021-01-12 15:25:46 +0100

    Implement Lobby source code

--- src/server/server/Lobby.cpp
+++ src/server/server/Lobby.cpp
@@ -2,7 +2,26 @@
 
 #include <boost/asio.hpp>
 #include <iostream>
+#include <list>
 
 using namespace server;
 using namespace std;
 using namespace boost;
+
+Lobby::Lobby() {
+
+}
+
+void Lobby::addClient(std::shared_ptr<server::Client> client) {
+	this->clients.push_back(client);
+	this->clients.unique();
+}
+
+void Lobby::removeClient(std::shared_ptr<server::Client> client) {
+	this->clients.remove(client);
+}
+
+std::set<std::string> Lobby::getClients() {
+	return this->clients;
+}
+
