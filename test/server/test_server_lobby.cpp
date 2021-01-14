d8b6ef9e9831fc779058e200381bf0915a43246d
Alban Benmouffek <albanben@protonmail.com>
2021-01-14 11:48:04 +0100

    Test the lobby

---
+++ test/server/test_server_lobby.cpp
@@ -0,0 +1,38 @@
+#include <boost/test/unit_test.hpp>
+#include <memory>
+#include <iostream>
+
+#include "../../src/server/server.h"
+
+using namespace server;
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(server_Lobby)
+
+BOOST_AUTO_TEST_CASE(Lobby_addAndRemoveClients){
+    shared_ptr<boost::asio::ip::tcp::socket> socket = make_shared<boost::asio::ip::tcp::socket>;
+    shared_ptr<Client> player1 = make_shared<Client>(string("My name"), socket);
+    shared_ptr<Client> player2 = make_shared<Client>(string("Your name"), socket);
+    shared_ptr<Lobby> lobby = make_shared<Lobby>;
+
+    BOOST_TEST(lobby->getClients().size() == 0);
+
+    lobby.addClient(player1);
+
+    BOOST_TEST(lobby->getClients().size() == 1);
+
+    lobby.removeClient(player2);
+
+    BOOST_TEST(lobby->getClients().size() == 1);
+
+    lobby.removeClient(player1);
+
+    BOOST_TEST(lobby->getClients().size() == 0);
+
+    lobby.addClient(player1);
+    lobby.addClient(player2);
+
+    BOOST_TEST(lobby->getClients().size() == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
