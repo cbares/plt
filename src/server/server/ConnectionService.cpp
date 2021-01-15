bdaa1bc37716cc1214e102966d67921fe3c76f0f
maxime <maximemarroufin@gmail.com>
2021-01-15 16:47:18 +0100

    Added ack

--- src/server/server/ConnectionService.cpp
+++ src/server/server/ConnectionService.cpp
@@ -8,10 +8,12 @@
 
 #include "network/UserNameMessage.h"
 #include "network/AckMessage.h"
+#include "network/Message.h"
 
 using namespace server;
 using namespace std;
 using namespace boost;
+using namespace network;
 
 void ConnectionService::startHandlingClient (std::shared_ptr<boost::asio::ip::tcp::socket> socket, std::shared_ptr<Lobby> lobby){
     std::thread th(([this, socket,lobby](){handleClient(socket,lobby);}));      
@@ -25,8 +27,13 @@ void ConnectionService::handleClient (std::shared_ptr<boost::asio::ip::tcp::sock
 		Json::Value response;
 		Json::Reader reader;
 		reader.parse(stream,response,false);
-		std::cout << "user \"" << response["userName"].asString() << "\" connected"<< endl;
-		lobby->addClient(std::make_shared<Client>(response["userName"].asString(),socket));
+		std::shared_ptr<UserNameMessage> userNameMessage = std::dynamic_pointer_cast<UserNameMessage>(Message::unserialize(response));
+		if(userNameMessage){
+			std::cout << "user \"" << userNameMessage->userName << "\" connected"<< endl;
+			lobby->addClient(std::make_shared<Client>(userNameMessage->userName,socket));
+			socket->send(asio::buffer(Message::format(make_shared<AckMessage>()->serialize())));
+		}
+
 	}
 	catch (system::system_error &e) {         
 		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
