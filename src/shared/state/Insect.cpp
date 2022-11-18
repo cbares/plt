2955adfebc7e4105a64375c3443d31173dff5ebc
Lavan <lavan4skr@gmail.com>
2022-11-18 10:02:54 +0100

    feat : updated insect.cpp & bee.cpp

--- src/shared/state/Insect.cpp
+++ src/shared/state/Insect.cpp
@@ -1,62 +1,65 @@
-//
-// Created by ensea on 30/10/22.
-//
 #include "Insect.hpp"
 #include "Case.hpp"
+#include <iostream>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
-using namespace state;
 
-Insect::Insect(std::string name, std::string color, std::vector<int> coord, int level) {
-    this->Name = name;
-    this->Color = color;
-    this->Coord = {-1,-1};
-    this->Level = 0;
-    this->IsPlaced = 0;
+void state::Insect::Update_Coord(std::vector<int> coord) {
+
+    this->Coord=coord;
+
 }
 
-string Insect::GetName() {
-    return this->Name;
-}
-
-string Insect::GetColor() {
-    return this->Color;
-}
-
-int Insect::GetLevel() {
-    return Level;
-}
-
-void Insect::SetPosition(std::vector<int> coord) {
-    this->Coord = coord;
-}
-
-void Insect::SetLevel(int level) {
-    this->Level = level;
-}
-
-bool Insect::GetIsPlaced() {
-    return this->IsPlaced;
-}
-
-void Insect::SetIsPlaced(bool state) {
-    this->IsPlaced = state;
-}
-
-std::vector<int> Insect::Get_Position() {
+std::vector<int> state::Insect::Get_Position() {
     return this->Coord;
 }
 
-std::vector<std::vector<int>>
-Insect::Possible_Placement_Insect(std::vector<Insect> list_insect_placed, std::vector<Case> list_case) {
+std::string state::Insect::GetName() {
+    return this->Name;
+}
+
+std::string state::Insect::GetColor() {
+    return this->Color;
+}
+
+int state::Insect::GetLevel() {
+    return this->Level;
+}
+
+void state::Insect::SetPosition(std::vector<int> coord) {
+    this->Coord=coord;
+}
+
+void state::Insect::SetLevel(int level) {   this->Level=level;
+}
+
+bool state::Insect::GetIsPlaced() {
+    return this->IsPlaced;
+}
+
+void state::Insect::SetIsPlaced(bool state) {
+    this->IsPlaced=state;
+}
+
+
+
+std::vector<std::vector<int>> state::Insect::Possible_Placement_Insect(vector<Insect> list_insect_placed,vector<Case> list_case) {
 
     std::vector<std::vector<int>> list_possible_placement;
     vector<Insect> Allies_Insect;
 
-    // Si aucun insecte est posé //on peut poser partout
+
+
+    // Si aucun insecte est posé
     if (list_insect_placed.size()==0){
         for (Case temp : list_case){
             list_possible_placement.push_back(temp.GetPosition());
+
         }
         return list_possible_placement;
     }
@@ -66,15 +69,98 @@ Insect::Possible_Placement_Insect(std::vector<Insect> list_insect_placed, std::v
     for (Insect temp : list_insect_placed){
         if(temp.GetColor()==this->Color) {
             Allies_Insect.push_back(temp);
+
         }
     }
 
-    //Si aucun insecte allié est posé //correspond à l'étape du 2ème joueur au tour 1
-    if (Allies_Insect.size()==0){ //donc il n'y a qu'un pion
+
+    //Si aucun insecte allié est posé
+
+    if (Allies_Insect.size()==0){
         for(Insect temp : list_insect_placed){
             int i = temp.Get_Position()[0];
             int j = temp.Get_Position()[1];
 
+            //Parité de j
+            if (j%2==0){
+                for (Case casetemp:list_case){
+                    if((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                }
+            }
+            else {
+                for (Case casetemp:list_case){
+                    if((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                    else if ((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                        list_possible_placement.push_back(casetemp.GetPosition());
+                    }
+                }
+            }
+        }
+        return list_possible_placement;
+    }
+
+
+    //Cette partie concerne donc les autres cas
+    for (Insect temp: Allies_Insect){
+        int i = temp.Get_Position()[0];
+        int j = temp.Get_Position()[1];
+
+
+        //Parité de j
+        if (j%2==0){
+            for (Case casetemp:list_case){
+                if((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+                else if((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+                else if ((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+                else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+                else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+                else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                    list_possible_placement.push_back(casetemp.GetPosition());
+                }
+            }
+        }
+        else {
             for (Case casetemp:list_case){
                 if((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
                     list_possible_placement.push_back(casetemp.GetPosition());
@@ -91,57 +177,74 @@ Insect::Possible_Placement_Insect(std::vector<Insect> list_insect_placed, std::v
                 else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
                     list_possible_placement.push_back(casetemp.GetPosition());
                 }
-                else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
+                else if ((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
                     list_possible_placement.push_back(casetemp.GetPosition());
                 }
             }
         }
-        return list_possible_placement;
-    }
 
-
-
-    //Cette partie concerne donc les autres cas
-    for (Insect temp: Allies_Insect){
-        int i = temp.Get_Position()[0];
-        int j = temp.Get_Position()[1];
-
-        for (Case casetemp:list_case){
-            if((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-            else if((casetemp.GetPosition()[0]==i-1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-            else if ((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j-1)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-            else if ((casetemp.GetPosition()[0]==i)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-            else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-            else if ((casetemp.GetPosition()[0]==i+1)&&(casetemp.GetPosition()[1]==j+1)&&(casetemp.GetEmpty()== true)){
-                list_possible_placement.push_back(casetemp.GetPosition());
-            }
-        }
     }
 
 //RECHERCHE DES DOUBLONS DANS LA LISTES DES COORDONNEES POSSIBLES
-    vector<vector<int>> list_possible_placement_unique;
-    list_possible_placement_unique.push_back(list_possible_placement[0]);
+    //vector<vector<int>> list_possible_placement_unique;
+    //list_possible_placement_unique.push_back(list_possible_placement[0]);
 
-    for (vector<int> temp:list_possible_placement){
-        for (vector<int> uniquemember : list_possible_placement_unique){
-            if ((temp[0]!=uniquemember[0])||(temp[1]!=uniquemember[1])){
-                list_possible_placement_unique.push_back(temp);
-            }
-        }
+
+
+    for (int i=0;i<list_possible_placement.size();i++){
+        cout << "Coord dispos :"<<list_possible_placement[i][0]<<" "<<list_possible_placement[i][1] << endl;
     }
+
+
+    /* for (vector<int> uniquemember: list_possible_placement_unique) {
+         cout << "new loop" << endl;
+         for (int i = 0; i < list_possible_placement_unique.size(); i++) {
+             cout << "Etat d'unique :" << list_possible_placement_unique[i][0] << " "
+                  << list_possible_placement_unique[i][1] << endl;
+         }
+         for (vector<int> temp: list_possible_placement) {
+             cout << "temp = " << temp[0] << temp[1] << endl;
+             cout << "candidat1 = " << uniquemember[0] << uniquemember[1] << endl;
+             if ((temp[0] != uniquemember[0]) || (temp[1] != uniquemember[1])) {
+                 cout << "IF OK" << endl;
+                 list_possible_placement_unique.push_back(temp);
+                 cout << "ajout de = " << temp[0] << temp[1] << endl;
+             }
+         }
+     }*/
+
+
+    vector<int> indicetosup;
+    vector<vector<int>> list_possible_placement_unique=list_possible_placement;
+    for (int i=0;i<list_possible_placement.size()-1;i++){
+        for (int j=i+1;j<list_possible_placement.size();j++){
+            auto temp = list_possible_placement[i];
+            auto candidat = list_possible_placement[j];
+            if ((temp[0] == candidat[0]) && (temp[1] == candidat[1])){
+                indicetosup.push_back(j);
+            }
+
+        }
+
+    }
+    std::sort(indicetosup.begin(), indicetosup.end());
+    std::unique(indicetosup.begin(), indicetosup.end());
+    for (int i=0;i<indicetosup.size();i++){
+        list_possible_placement_unique.erase(list_possible_placement_unique.begin()+indicetosup[i]-i);
+    }
+
+
+
+    cout <<"OK FINAL"<< endl;
     return list_possible_placement_unique;
 }
 
+std::vector<std::vector<int>>
+state::Insect::Possible_Deplacement_Insect(vector<Insect> list_insect_placed, vector<Case> list_case) {
+    return std::vector<std::vector<int>>();
+}
 
+state::Insect::Insect(std::string name, std::string color, std::vector<int> coord, int level) {
+    this->Name=name;this->Color=color;this->Coord=coord;this->Level=level;this->IsPlaced=false;
 
-
+}
