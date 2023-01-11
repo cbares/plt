f8913ca47e2d4e2d77c2354e02493eb0e2c35ebb
Alith <ali.zouhairi@laposte.net>
2023-01-11 16:22:12 +0100

    IsBeeCircled update

--- src/shared/state/Game.cpp
+++ src/shared/state/Game.cpp
@@ -46,50 +46,70 @@ bool state::Game::IsBeeCircled(state::Player player) {
 
     bool result;
 
-    if (player.GetColor()=="White"){
+    if (player.GetColor() == "White") {
 
-        for (auto bee : player.Get_List_Insect_Played()){
-            if (bee->GetName()=="Bee_B"){
-                int i = bee->Get_Position()[0];int j=bee->Get_Position()[1];
-                if(j%2==0){
-                    if((GameMap->GetListCase()[i][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i-1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j+1].GetEmpty()== false)){
+        for (auto bee: player.Get_List_Insect_Played()) {
+            if (bee->GetName() == "Bee_B") {
+                int i = bee->Get_Position()[0];
+                int j = bee->Get_Position()[1];
+                if (j % 2 == 0) {
+                    if ((GameMap->GetListCase()[i][j - 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i][j + 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i - 1][j].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i + 1][j - 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i + 1][j].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i + 1][j + 1].GetEmpty() == false)) {
                         result = true;
                     }
-                }
-                else{
-                    if((GameMap->GetListCase()[i-1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i-1][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)){
+                } else {
+                    if ((GameMap->GetListCase()[i - 1][j].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i - 1][j + 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i][j - 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i][j + 1].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i + 1][j].GetEmpty() == false) &&
+                        (GameMap->GetListCase()[i][j + 1].GetEmpty() == false)) {
                         result = true;
+                    } else {
+                        result = false;
                     }
-                }
-
-                else {
-                    result= false;
                 }
             }
         }
     }
 
-    if (player.GetColor()=="Black"){
-        for (auto bee : player.Get_List_Insect_Played()){
-            if (bee->GetName()=="Bee_A"){
-                int i = bee->Get_Position()[0];int j=bee->Get_Position()[1];
-                if(j%2==0){
-                    if((GameMap->GetListCase()[i][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i-1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j+1].GetEmpty()== false)){
-                        result = true;
-                    }
-                }
-                else{
-                    if((GameMap->GetListCase()[i-1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i-1][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j-1].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)&&(GameMap->GetListCase()[i+1][j].GetEmpty()== false)&&(GameMap->GetListCase()[i][j+1].GetEmpty()== false)){
-                        result = true;
-                    }
-                }
 
-                else {
-                    result= false;
+            if (player.GetColor() == "Black") {
+                for (auto bee: player.Get_List_Insect_Played()) {
+                    if (bee->GetName() == "Bee_A") {
+                        int i = bee->Get_Position()[0];
+                        int j = bee->Get_Position()[1];
+                        if (j % 2 == 0) {
+                            if ((GameMap->GetListCase()[i][j - 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i][j + 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i - 1][j].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i + 1][j - 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i + 1][j].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i + 1][j + 1].GetEmpty() == false)) {
+                                result = true;
+                            }
+                        } else {
+                            if ((GameMap->GetListCase()[i - 1][j].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i - 1][j + 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i][j - 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i][j + 1].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i + 1][j].GetEmpty() == false) &&
+                                (GameMap->GetListCase()[i][j + 1].GetEmpty() == false)) {
+                                result = true;
+                            }
+
+
+                        else {
+                            result = false;
+                        }
+                        }
+                    }
                 }
             }
-        }
-    }
 
     return result;
 }
